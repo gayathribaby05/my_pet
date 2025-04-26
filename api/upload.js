bce5088a707708faacf08056fff8a32f830d4e5c
Yakkala Baby Lakshmi Gayathri <145356600+gayathribaby05@users.noreply.github.com>
2025-04-26 13:21:26 +0530

    Update the file with the generated Content

--- api/upload.js
+++ api/upload.js
@@ -1,11 +1,35 @@
-export default async function handler(req, res) {
-    if (req.method === "POST") {
-      const { title, description, image } = req.body;
-  
-      console.log("Received data:", { title, description, image });
-  
-      return res.status(200).json({ message: "Data received!" });
-    }
-  
-    return res.status(405).json({ message: "Method not allowed" });
-  }
+Here's a blog post based on the text and image URLs you provided:
+
+**Celebrating the Finalists of the ReTech Startup Awards 2024**
+
+The ReTech Startup Awards 2024 has announced its list of finalists, and we are thrilled to celebrate the outstanding achievements of these innovative companies. The ReTech Startup Awards recognize and reward entrepreneurs and startups who are pushing the boundaries of innovation in the retail technology space.
+
+**Meet the Finalists**
+
+We are proud to announce the following companies that have made it to the finals:
+
+• iamX Retail Pvt. Ltd.
+• [Company Name 2]
+• [Company Name 3]
+• [Company Name 4]
+• OfIntelligence Technologies Pvt. Ltd.
+• [Company Name 5]
+• Insights LLP
+• [Company Name 6]
+• Technology Pvt. Ltd.
+
+These finalists have demonstrated exceptional innovation, entrepreneurship, and dedication to disrupting the retail technology landscape.
+
+**About iamX Retail Pvt. Ltd.**
+
+iamX Retail Pvt. Ltd. is a retail technology company that provides innovative solutions to retailers. With a focus on customer experience, iamX helps retailers create personalized interactions with their customers, driving loyalty and increasing sales.
+
+[Image URL: https://media.licdn.com/dms/image/v2/D560BAQF45cp_RFZpnQ/company-logo_100_100/company-logo_100_100/0/1715885396777/iamx_retail_logo?e=1750896000&v=beta&t=MnLBchxb3zD52O7H0wDKTjX_PbDKBiMJvZkA5oTOL5M]
+
+**Conclusion**
+
+The ReTech Startup Awards 2024 is a testament to the innovative spirit and entrepreneurial drive of the retail technology community. We congratulate all the finalists and wish them the best of luck in the final round of the competition. The winners will be announced soon, and we look forward to celebrating their achievements.
+
+Stay tuned for more updates on the ReTech Startup Awards 2024 and the retail technology industry.
+
+Note: You can add more information about each company, their innovative solutions, and their achievements in the retail technology space. You can also add more images and logos of the finalist companies.
